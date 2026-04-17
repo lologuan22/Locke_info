2d323bb8167f1b8f868432c653cb3e5d2cdefbb1
Xiong <180499566+songtaoheying@users.noreply.github.com>
2026-04-17 20:43:35 +0800

    初始化后端项目

---
+++ backend/locke/src/main/java/com/newblash/locke/LockeApplication.java
@@ -0,0 +1,13 @@
+package com.newblash.locke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockeApplication.class, args);
+	}
+
+}
