ef9f6909ce5865bbe6580c849f486a724e8d4d40
Xiong <180499566+songtaoheying@users.noreply.github.com>
2026-04-27 17:35:35 +0800

    更新自增id

--- backend/locke/src/main/java/com/newblash/locke/entity/User.java
+++ backend/locke/src/main/java/com/newblash/locke/entity/User.java
@@ -1,6 +1,9 @@
 package com.newblash.locke.entity;
 
 import java.time.LocalDateTime;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -9,7 +12,8 @@ import lombok.Data;
 @TableName("users")
 @Schema(description = "用户信息实体")
 public class User {
-
+    
+    @TableId(type = IdType.AUTO)
     @Schema(description = "用户ID", example = "1")
     private Integer id;
 
