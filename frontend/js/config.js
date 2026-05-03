6225ed967d9ff53fe33dd170037e1f085d27b2a4
Xiong <180499566+songtaoheying@users.noreply.github.com>
2026-05-03 21:06:13 +0800

    重写拦截器

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,7 +1,8 @@
 export const CONFIG = {
      
     // BASE_URL: 'https://project.ash-xiong.de5.net',
-    BASE_URL: 'http://localhost:8080',
+    // BASE_URL: 'http://localhost:8080',
+    BASE_URL: '',
     TIMEOUT: 10000
 };
 
